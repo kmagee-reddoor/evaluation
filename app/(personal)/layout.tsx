e95d4632f9553abe7cb5e7c58514019409ffb711
Kyle Magee <kmagee@reddoor.biz>
2024-05-24 11:18:44 -0700

    removes intro template

--- app/(personal)/layout.tsx
+++ app/(personal)/layout.tsx
@@ -60,7 +60,6 @@ export default async function IndexRoute({
         <Suspense>
           <Footer />
         </Suspense>
-        <Suspense><IntroTemplate /></Suspense>
       </div>
       {draftMode().isEnabled && <LiveVisualEditing />}
     </>

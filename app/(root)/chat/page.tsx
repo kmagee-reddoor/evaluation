0130416f987cb2ee5023a31949fa61c04d08ce24
kmagee-reddoor <kmagee@reddoor.biz>
2025-07-11 17:39:49 -0700

    fixes page

--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -1,13 +1,4 @@
-import { Header } from '@/components/shared/Header'
-import type { PagePayload } from '@/types'
-
-export interface PageProps {
-  data: PagePayload | null
-}
-
-export function Page({ data }: PageProps) {
-  // Default to an empty object to allow previews on non-existent documents
-  const { body, overview, title } = data ?? {}
+export function Page() {
 
   return (
     <div>
